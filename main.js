68643e471b39a98540e8f48516269538d1c74fcc
Shako Eliadze <sgeanonym@gmail.com>
2023-03-01 19:49:05 +0400

    added dark mode
    started learning AJAX calls <3

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+'use strict'
+
+const body = document.querySelector('body');
 const upperPanel = document.querySelector('.upper-panel');
 const homeFullLeftSide = document.querySelector('.home-left-side');
 const navbarCategories = document.querySelector('.navbar-categories');
 const navbarCategoriesItem = document.querySelector('.navbar-categories-item');
 const mainCategories = document.querySelector('.main-categories');
-const mainCategoriesItem = document.querySelector('.main-categories-item');
 const upperPanelHeader = document.querySelector('.upper-panel-header');
 const upperPanelHeaderItem = document.querySelector('.upper-panel-header-item');
 const classLogo = document.querySelector('.logo');
@@ -36,12 +38,8 @@ categories.id.forEach(id => {
   mainCategories.insertAdjacentHTML('beforeend', addItem);
 })
 
-// newCollection.style.backgroundImage = 'url("/images/new-collection.jpg")'
-// newCollection.style.backgroundSize = "cover";
-// newCollection.style.backgroundRepeat = "no-repeat";
-// newCollection.style.backgroundPosition = "center center";
 
-
+const mainCategoriesItem = document.querySelectorAll('.main-categories-item');
 
 const addNewCollection = function (name, f, img) {
   const html = `<div class="new-collection">
@@ -51,17 +49,93 @@ const addNewCollection = function (name, f, img) {
   </div>
 </div>`;
 
+
   section.insertAdjacentHTML('beforeend', html);
   const newCollection = document.querySelector('.new-collection');
-
 
   newCollection.style.backgroundImage = `url(${img})`;
   newCollection.style.backgroundSize = "cover";
   newCollection.style.backgroundRepeat = "no-repeat";
   newCollection.style.backgroundPosition = "center center";
+  return newCollection;
 }
 
-addNewCollection("Arrakis's Hero", "Man, Woman", './images/new-collection.jpg');
+const collectionArrakis = addNewCollection("Arrakis's Hero", "Man, Woman", './images/new-collection.jpg');
+const newsPage = document.querySelectorAll('.main-categories-item')[0];
 
 
+let i = 0;
+classLogo.addEventListener('click', () => {
+  if (i === 0) {
+    i++;
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+    mainCategories.style.color = 'white';
+    body.style.transition = '1s'
+  } else {
+    i--;
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';
+    mainCategories.style.color = 'var(--least-text-color)';
+    body.style.transition = '1s'
+  }
+})
 
+
+// newsPage.addEventListener('click', function () {
+//   // Delete middle line
+//   upperPanelHeaderCTA.style.display = 'none';
+//   collectionArrakis.style.display = 'none';
+
+//   // add middle layout
+
+
+// })
+
+
+// Learning AJAX Calls <3 everything for this project bitchaaaaaaaaaaas!
+
+// 'https://dummyjson.com/products'
+
+
+// const getProducts = function (url) {
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//       return data;
+//     })
+// }
+
+// const allProducts = getProducts('https://dummyjson.com/products');
+// console.log(allProducts);
+
+
+// const request = fetch('https://dummyjson.com/products');
+// console.log(request);
+
+// const getProducts = function (url) {
+//   fetch(url)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     })
+// }
+
+const getProducts = function (url) {
+  const data = fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data.products;
+    });
+  return data;
+}
+
+
+const data0 = getProducts('https://dummyjson.com/products');
+console.log(data0);
+data0.then(data => console.log(data[0].title));
